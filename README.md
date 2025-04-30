ed716e8bc8d5987f2a27af7b3929e027318a095c
bytrustu <39726717+bytrustu@users.noreply.github.com>
2025-04-30 20:32:53 +0900

    feat: 홈, 조인 페이지 추가

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 의식적인 연습 Meetup
 
 <div align="left">
-  <img src="https://i.imgur.com/8eJYJOg.png" alt="thumbnail" width="600"/>
+  <img src="https://i.imgur.com/iEw8W5N.gif" alt="thumbnail" width="600"/>
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 4173,
+  },
 });
